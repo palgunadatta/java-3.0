b559c9b4f319efa371814f752096348cc74a87a9
palgunadatta <127437421+palgunadatta@users.noreply.github.com>
2023-09-29 15:13:17 +0530

    Update jfrog.py

--- jfrog.py
+++ jfrog.py
@@ -1,19 +1,22 @@
 import requests
 
 def jfrog():
-    artifactory_url = 'http://54.241.71.156:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    artifactory_url = 'http://54.177.86.80:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
 
-    path = '/home/ubuntu/JAVA_APP_3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+    file_path = '/home/ubuntu/java-3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
 
     username = 'admin'
     password = 'Datta123@'
 
-    with open(path , 'rb') as file:
-        reponse = requests.put(artifactory_url , auth=(username , password), data = file)
-    
-    if reponse.status_code == 201:
+    with open(file_path , 'rb') as file:
+        response = requests.put(artifactory_url , auth=(username , password), data = file)
+
+    if response.status_code == 201:
         print("\n PUT request successful")
     else:
-        print("PUT rquest failed with status code (response.status_code)")
+        print("PUT request failed with status code", response.status_code)
         print("response content:")
         print(response.text)
+
+# Call the jfrog function to execute the upload
+jfrog()
